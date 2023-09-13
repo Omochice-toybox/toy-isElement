9f79eccb73bc6f4a7d455213342e86b37160b70f
Omochice <mochice.mls.ntl@gmail.com>
2023-09-13 19:27:02 +0900

    test: Add test that element is in iframe

--- src/isElement.ts
+++ src/isElement.ts
@@ -13,10 +13,28 @@ if (import.meta.vitest) {
       expect(el instanceof HTMLDivElement).toBe(true);
       expect(isElement(el, "div")).toBe(true);
     });
+
     it("should return false", () => {
       const el = document.createElement("div") as HTMLElement;
       expect(el instanceof HTMLAnchorElement).toBe(false);
       expect(isElement(el, "a")).toBe(false);
+    });
+
+    it("should return true even if it in iframe element", () => {
+      const el = document.createElement("iframe");
+      document.body.appendChild(el);
+      const doc = el.contentDocument!;
+      expect(doc).toBeTruthy();
+
+      const div = doc.createElement("div");
+      expect(div instanceof HTMLDivElement).toBe(false);
+
+      doc.body.appendChild(div);
+      const view = div.ownerDocument.defaultView!;
+      expect(view).toBeTruthy();
+      expect(div instanceof view.HTMLDivElement).toBe(true);
+
+      expect(isElement(div, "div")).toBe(true);
     });
   });
 }
