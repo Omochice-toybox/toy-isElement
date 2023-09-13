a223d82c1c59aa8e9486e7c70b8680becd383ca1
Omochice <mochice.mls.ntl@gmail.com>
2023-09-13 17:59:59 +0900

    style: fmt

--- src/isElement.ts
+++ src/isElement.ts
@@ -5,19 +5,18 @@ export function isElement<T extends keyof HTMLElementTagNameMap>(
   return element.tagName.toLowerCase() === tagName.toLowerCase();
 }
 
-
 if (import.meta.vitest) {
-  const { describe, it, expect } = import.meta.vitest
+  const { describe, it, expect } = import.meta.vitest;
   describe("sample", () => {
     it("should return true", () => {
-      const el = document.createElement("div") as HTMLElement
-      expect(el instanceof HTMLDivElement).toBe(true)
-      expect(isElement(el, "div")).toBe(true)
-    })
+      const el = document.createElement("div") as HTMLElement;
+      expect(el instanceof HTMLDivElement).toBe(true);
+      expect(isElement(el, "div")).toBe(true);
+    });
     it("should return false", () => {
-      const el = document.createElement("div") as HTMLElement
-      expect(el instanceof HTMLAnchorElement).toBe(false)
-      expect(isElement(el, "a")).toBe(false)
-    })
-  })
+      const el = document.createElement("div") as HTMLElement;
+      expect(el instanceof HTMLAnchorElement).toBe(false);
+      expect(isElement(el, "a")).toBe(false);
+    });
+  });
 }
